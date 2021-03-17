0838a77b19bf2b0b2a2f4ef8319c1f9dd713dec3
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-03-17 21:17:29 +0800

    update attack.cpp

--- Project1-DNS-Reflection-and-Amplification-Attacks/src/attack.cpp
+++ Project1-DNS-Reflection-and-Amplification-Attacks/src/attack.cpp
@@ -22,15 +22,6 @@ struct UDP_PSD_Header{
 	u_int16_t len;
 };
 
-// unsigned short checksum(unsigned short *buff, int _16bitword){
-// 	unsigned long sum;
-// 	for (sum = 0; _16bitword > 0; _16bitword--)
-// 		sum += htons(*(buff)++);
-// 	sum = ((sum >> 16) + (sum & 0xFFFF));
-// 	sum += (sum >> 16);
-// 	return (unsigned short)(~sum);
-// }
-
 unsigned short csum(unsigned short *buf, int nwords){
 	unsigned long sum;
 	for (sum = 0; nwords > 0; nwords--)
