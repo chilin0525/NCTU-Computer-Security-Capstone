d3d53e484a674892a346322a2a840f10c9f7812c
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-04-22 23:10:39 +0800

    update mitm_attack.py

--- Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/mitm_attack.py
+++ Project2-MITM-and-Pharming-Attacks-in-Wi-Fi-Networks/mitm_attack.py
@@ -113,7 +113,7 @@ if __name__ == "__main__":
                     for line in f:
                         if ("username=" in line) and ("password=" in line):
                             (username, passwd) = re.findall("username=(.*?)&password=(.*?)&captcha_code=HTTP/1.1 303 See Other", line)[0]
-                            print("username: ", username, " password: ",  passwd)
+                            print("username: ", username.encode("utf-8"), " password: ",  passwd)
             sending_flag = False
             sleep(1)
     finally:
